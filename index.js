52814941ec9654d438840b5929731a2630498e51
priegelsdev <priegelsdev@gmail.com>
2022-12-29 13:03:53 +0100

    refactor loop to map function for dice rolls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hero = {
     name: "Marine",
     avatar: "img/marine.jpg",
     health: 60,
-    diceScore: 6,
+    diceScore: [3, 1, 4],
     diceCount: 3
 }
 
@@ -12,7 +12,7 @@ const monster = {
     name: "Space Worm",
     avatar: "img/worm.jpg",
     health: 10,
-    diceScore: 4,
+    diceScore: [6],
     diceCount: 1
 }
 
@@ -22,10 +22,8 @@ function renderCharacter(char) {
     // object destructuring
     const {elementId, name, avatar, health, diceScore, diceCount} = char;
 
-    let diceHtml = '';
-    for (let i = 0; i < diceCount; i++) {
-        diceHtml += `<div class="dice">${diceScore}</div>`
-    }
+    // mapping over dice array instead of looping
+    const diceHtml = diceScore.map(num => `<div class="dice">${num}</div>`).join('');
 
     document.getElementById(elementId).innerHTML =
     `<div class="character-card">
