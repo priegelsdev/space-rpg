2ed7ea936314d28a6762adf776554c5598454261
priegelsdev <priegelsdev@gmail.com>
2022-12-29 13:42:37 +0100

    write getDiceRollArray fn and getDiceHtml

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const hero = {
     name: "Marine",
     avatar: "img/marine.jpg",
     health: 60,
-    diceScore: [3, 1, 4],
     diceCount: 3
 }
 
@@ -12,18 +11,32 @@ const monster = {
     name: "Space Worm",
     avatar: "img/worm.jpg",
     health: 10,
-    diceScore: [6],
     diceCount: 1
+}
+
+function getDiceRollArray(diceCount) {
+    let diceRollArray = [];
+    for (let i = 0; i < diceCount; i++) {
+        let randomNum = Math.floor(Math.random() * 6) + 1;
+        diceRollArray.push(randomNum);
+    }
+    return diceRollArray;
+}
+
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(
+        num => `<div class="dice">${num}</div>`
+    ).join('');
 }
 
 // function to render out character data
 function renderCharacter(char) {
 
     // object destructuring
-    const {elementId, name, avatar, health, diceScore, diceCount} = char;
+    const {elementId, name, avatar, health, diceCount} = char;
 
     // mapping over dice array instead of looping
-    const diceHtml = diceScore.map(num => `<div class="dice">${num}</div>`).join('');
+    const diceHtml = getDiceHtml(diceCount);
 
     document.getElementById(elementId).innerHTML =
     `<div class="character-card">
