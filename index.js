86cd16b176e04e3688d379842b5ff9cb760ded6a
priegelsdev <priegelsdev@gmail.com>
2023-01-01 11:26:08 +0100

    testing out different ways

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import {charData} from './data.js'
-import Character from './Character.js'
+import {charData} from './data.js';
+import Character from './Character.js';
+import { getPlaceholderHtml } from './utils.js';
 
 const marine = new Character(charData[0]);
 const spaceWorm = new Character(charData[1]);
@@ -7,23 +8,33 @@ const spaceWorm = new Character(charData[1]);
 // event listener
 document.querySelector('.attack-btn').addEventListener('click', handleAttack);
 
-// function to display placeholder dice on initial page load
-
-function getPlaceholderHtml() {
-    const diceArr = document.querySelectorAll('.dice')
-    diceArr.forEach(dice => clear(dice)
-        )
-
-    // function to clear dice scores and make background transparent
-        function clear(dice) {
-            dice.innerHTML = '';
-            dice.classList.add('placeholder-dice')
-        }
-}
-
 // function to  handle attack
 function handleAttack() {
     render();
+
+    const diceArr = document.querySelectorAll('.dice')
+    let diceTotal = 0;
+
+    diceArr.forEach(dice => countTotal(dice)
+        )
+
+    // function to add dice of character to total
+        function countTotal(dice) {
+ 
+            if (dice.classList.contains('placeholder-dice')) {
+                console.log('test');
+            } else {
+                const diceFloat = parseFloat(dice.innerText)
+                diceTotal += diceFloat; 
+                console.log(diceTotal)}
+        }   
+
+    console.log(diceTotal)
+    spaceWorm.health -= diceTotal;
+    console.log(spaceWorm.health)
+
+
+   
 }
 
 //function to render out characters with constructor fn method
--- utils.js
+++ utils.js
@@ -4,4 +4,18 @@ function getDiceRollArray(diceCount) {
     return new Array(diceCount).fill(0).map(() => Math.floor(Math.random() * 6 + 1)) 
 }
 
-export {getDiceRollArray} 
+// function to display placeholder dice on initial page load
+
+function getPlaceholderHtml() {
+    const diceArr = document.querySelectorAll('.dice')
+    diceArr.forEach(dice => clear(dice)
+        )
+
+    // function to clear dice scores and make background transparent
+        function clear(dice) {
+            dice.innerHTML = '';
+            dice.classList.add('placeholder-dice')
+        }
+}
+
+export {getDiceRollArray, getPlaceholderHtml} 
