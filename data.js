ae75c275a1a45cd5d62259bf99003cdf1aaa349a
priegelsdev <priegelsdev@gmail.com>
2023-01-04 12:38:58 +0100

    add two characters

--- data.js
+++ data.js
@@ -1,20 +1,36 @@
-const charData = [{
-    elementId: "hero",
+const charData = {
+  hero: { 
     name: "Marine",
     avatar: "img/marine.jpg",
     health: 60,
     diceCount: 3,
     currentDiceScore: [0],
     isAlive: true
-},
-{
-    elementId: "monster",
+  },
+  spaceworm: {
     name: "Space Worm",
     avatar: "img/worm.jpg",
-    health: 40,
+    health: 20,
     diceCount: 1,
     currentDiceScore: [0],
     isAlive: true
-}]
+  },  
+  mutant: {
+    name: "El Mutante",
+    avatar: "img/mutant.jpg",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: [0],
+    isAlive: true
+  },
+  alien: {
+    name: "Big Al",
+    avatar: "img/alien.jpg",
+    health: 30,
+    diceCount: 3,
+    currentDiceScore: [0],
+    isAlive: true 
+  }
+}
 
 export {charData}
